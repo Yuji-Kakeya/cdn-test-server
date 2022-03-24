86212d8fbcc78ad378ba3633df5bd9cba0470acc
Yuji Kakeya <12113@pn.macnica.co.jp>
2022-03-24 18:42:44 +0900

    wildcard

--- dist/start.sh
+++ dist/start.sh
@@ -5,7 +5,8 @@ ST=example
 L=example
 O=example
 
-echo "subjectAltName=DNS:$1" > ./dist/cert/_san.conf
+WILDCARD=`echo $1 | sed -e "s/^[^.]*/*/g"`
+echo "subjectAltName=DNS:$1, DNS:${WILDCARD}" > $1/_san.conf
 openssl genrsa -passout pass:${PASSWORD} -aes256 -out ./dist/cert/_server.key 2048
 openssl rsa -passin pass:${PASSWORD} -in ./dist/cert/_server.key -out ./dist/cert/server.key
 openssl req -new -key ./dist/cert/server.key -out ./dist/cert/_request.csr -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/CN=$1"
